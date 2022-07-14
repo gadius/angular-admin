025960fba8892e3ff2335474b1e44087c552c575
Gadius <gadlapaz@hotmail.com>
2022-07-13 19:19:58 -0600

    Login form and router to welcome basic view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './admin/welcome/welcome.component';
+import { LoginComponent } from './auth/login/login.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'welcome', component: WelcomeComponent },
+
+  { path: '**', redirectTo: '/login' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  private initForm(){
+    this.loginForm = new FormGroup({
+      username: new FormControl(null, Validators.required),
+      password: new FormControl(null, Validators.required),
+    });
+  }
+
+  onSubmit(){
+    this.router.navigate(['welcome']);
+  }
+
+}
