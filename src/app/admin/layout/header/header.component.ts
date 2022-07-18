9ca58310a6649a2a36e85c90a57c35b5c46efd62
Gadius <gadlapaz@hotmail.com>
2022-07-17 22:42:12 -0600

    Auth Guard added to redirect login

--- src/app/admin/layout/header/header.component.ts
+++ src/app/admin/layout/header/header.component.ts
@@ -27,7 +27,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   onLogout(){
     this.authService.logout();
-    this.router.navigate(['login']);
+    //this.router.navigate(['login']);
 
   }
 
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { UsersComponent } from './admin/users/users.component';
 import { WelcomeComponent } from './admin/welcome/welcome.component';
+import { AuthGuard } from './auth/login/auth.guard';
 import { LoginComponent } from './auth/login/login.component';
 
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'admin', component: AdminComponent,
+  { path: 'admin', component: AdminComponent, canActivate: [AuthGuard],
     children: [
       { path: 'welcome', component: WelcomeComponent },
       { path: 'users', component: UsersComponent },
