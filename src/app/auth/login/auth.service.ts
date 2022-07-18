c719f0791f6823041ad8959b81fa1793c286f90a
Gadius <gadlapaz@hotmail.com>
2022-07-17 22:08:43 -0600

    autologout timer

--- src/app/auth/login/auth.service.ts
+++ src/app/auth/login/auth.service.ts
@@ -15,6 +15,7 @@ import { UserModel } from './user.model';
 export class AuthService {
   //user = new Subject<UserModel>; SE USARÁ MEJOR BEHAVIORSUBJECT PARA OBTENER EL VALOR INCLUSIVE SI AL MOMENTO DE SUSCRIBIRSE DE OTRO COMPONENTE TENIA OTRO VALOR
   user = new BehaviorSubject<UserModel | null>(null);
+  private tokenExpirationTimer: any;
 
   is_logged_in: boolean = false;
   base_url: string = 'http://angular-api.test/api/';
@@ -39,6 +40,11 @@ export class AuthService {
     this.is_logged_in = false;
     this.user.next(null);
     localStorage.removeItem('userData');
+    if(this.tokenExpirationTimer)
+    {
+      clearTimeout(this.tokenExpirationTimer);
+    }
+    this.tokenExpirationTimer = null;
   }
 
   autoLogin() {
@@ -48,12 +54,20 @@ export class AuthService {
       return;
     }
     this.user.next(userData);
+    this.autoLogout(20000);
+  }
+
+  autoLogout(expirationDuration: number){
+    this.tokenExpirationTimer = setTimeout(() => {
+      this.logout();
+    }, expirationDuration);
   }
 
   private handleAuthentication(email: string, id: string, token: string) {
     let user_aux = new UserModel(email, id, token);
     this.user.next(user_aux);
     localStorage.setItem('userData', JSON.stringify(user_aux));
+    this.autoLogout(20000);
   }
 
   private handleError(errorRes: HttpErrorResponse) {
